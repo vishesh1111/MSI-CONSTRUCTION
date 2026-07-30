ee64d80c79edc3f9ffb0df9e96514b4a647e9401
vishesh1111 <vvishesh53@gmail.com>
2026-07-30 20:59:54 +0530

    Update navbar logo to MSI.png

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
           <div className={styles.container}>
             <Link href="/" className={styles.logo}>
               <Image 
-                src="/img/LOGO33_trimmed.png" 
+                src="/img/MSI_cropped.png" 
                 alt="MSI Construction" 
                 width={160} 
                 height={60} 
