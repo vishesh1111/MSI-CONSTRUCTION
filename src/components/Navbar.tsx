a9657fea753c6eeb032e32912fab42e116ba03ea
vishesh1111 <vvishesh53@gmail.com>
2026-07-30 19:15:08 +0530

    Zoom LOGO11 further in navbar and favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
           <div className={styles.container}>
             <Link href="/" className={styles.logo}>
               <Image 
-                src="/img/LOGO11_cropped.png" 
+                src="/img/LOGO11_zoomed_more.png" 
                 alt="MSI Construction" 
                 width={160} 
                 height={60} 
