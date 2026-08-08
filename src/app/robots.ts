9922cc2b5a16264719f97fff81f2520888d6c66c
vishesh1111 <vvishesh53@gmail.com>
2026-08-08 11:08:26 +0530

    fixed google crawlBOT problem

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin', '/admin/*', '/api/', '/api/*', '/_next/'],
+      disallow: ['/admin', '/admin/*', '/api/', '/api/*'],
     },
     sitemap: 'https://www.msiconstruction.in/sitemap.xml',
   };
